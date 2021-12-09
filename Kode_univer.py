9c027f0311743b3ce78a7b99401d02a824df20b4
Eugene Linkevich <linkevich.ev@yandex.ru>
2021-12-09 15:17:14 +0300

    Add VK api message

--- Kode_univer.py
+++ Kode_univer.py
@@ -1,11 +1,43 @@
+import requests
+import pwd_vk
 
+token = pwd_vk.access_token
+version = pwd_vk.v
 
-#input message
-a = input('140 letters')
-print(a)
+def message_in(message_user):
+    mes_out = message_user[:140]
+    return mes_out
 
-print('see you')
-print('Don\'t see you!')
+def message_post_vk(message_user, token, version):
+    return requests.post(f'https://api.vk.com/method/wall.post?message={message_user}&access_token={token}&v={version}')
 
 if __name__ == '__main__':
-    print('Working')
+
+    # input message
+    message_user = input()
+
+    # check message length
+    if len(message_user) > 140:
+        print('Сообщение должно содержать не более 140 символов.')
+        print('Длина сообщения: ' + str(len(message_user)) + '.')
+        print('Будет опубликованно сообщение: \"' + message_in(message_user) + '\".')
+
+    # trim message to 140 characters
+    message_user = message_in(message_user)
+    # print(len(message_user))
+
+    # posting message on VKontakte
+    try:
+        mymes = message_post_vk(message_user, token, version)
+        print(mymes)
+        print('Сообщение опубликовано.')
+    except ConnectionError:
+        print('Ошибка подключения.')
+
+
+
+
+
+
+
+
